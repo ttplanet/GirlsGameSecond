af0fee010461fa2a807379d35eb50b0f2ea56594
ttplanet <twintailsbeadds32@gmail.com>
2018-05-04 12:15:07 +0900

    Managerクラス追加　UI Scene

---
+++ Assets/Script/Scene/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace GirlsGameSecond.Scene
+{
+    public class SceneManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        public void SceneLoad(string sceneName)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
+
---
+++ Assets/Script/UI/UIManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GirlsGameSecond.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+
+    }
+}
